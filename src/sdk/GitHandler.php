6240fc4912703e5e68aed7b6e737741f89045465
IvanHunters <ohotnikovivan@yandex.ru>
2020-12-13 14:38:24 +0300

    add anon func

--- src/sdk/GitHandler.php
+++ src/sdk/GitHandler.php
@@ -10,14 +10,77 @@ class GitHandler
      * @var string
      */
     private string $branch;
+    private $debug = true;
 
-    public function __construct(string $branchName){
+    public function __construct(string $branchName, bool $debug = false){
+        $this->debug = $debug;
         $this->branch = $branchName;
         $status = [];
         exec("git status 2>&1", $status);
-        if ($status[0] === "On branch main") {
-
+        if ($status[0] === "On branch " . $branchName) {
+        }else{
+            throw new \RangeException("Please, run git init");
         }
-        $a = 10;
+    }
+
+    public function getDiff(array  $config, $beforeHandler, $afterHandler)
+    {
+        $diff = "git diff origin/".$this->branch."..".$this->branch." --name-only 2>&1";
+        $output = [];
+        $filesMap = [];
+
+        exec($diff, $output);
+        $files = $output;
+        if (count($output) == 0) {
+            return false;
+        }
+
+        $filesMap= $this->getDifFiles($files);
+        return $this->handlePullData($filesMap, $beforeHandler, $afterHandler);
+    }
+
+    private function getDifFiles(array $files): array
+    {
+        $filesMap = [];
+        foreach ($files as $file) {
+            $explodeFilePath = explode("/", $file);
+            $countExplode = count($explodeFilePath);
+            if ($countExplode > 0) {
+                $fileName = $explodeFilePath[$countExplode - 1];
+                unset($explodeFilePath[$countExplode - 1]);
+
+                $filePath = "./" . implode('/', $explodeFilePath);
+                $filesMap[$filePath][] = $fileName;
+            } else {
+                $filePath = "./";
+                $filesMap[$filePath][] = $file;
+            }
+        }
+        return $filesMap;
+    }
+
+    private function handlePullData($filesMap, $beforeHandler, $afterHandler) {
+        if (count($filesMap) > 1 || count($filesMap["./"]) > 1) {
+            $resultBefore = $beforeHandler($filesMap);
+            if ($resultBefore) {
+                $pullResult = $this->execPull();
+                if ($pullResult[0] === "Already up to date!" || $pullResult[0] === "Already up to date.") {
+                    return false;
+                }
+            }
+            $resultAfter = $afterHandler($filesMap);
+        }
+        return true;
+    }
+
+    private function execPull() {
+        $result = [];
+        exec("git pull origin " . $this->branch ." 2>&1", $result);
+        return $result;
+    }
+
+    private function logger(array $info) {
+        if ($this->debug)
+            echo implode("\n",$info);
     }
 }
--- src/sdk/Init.php
+++ src/sdk/Init.php
@@ -3,30 +3,25 @@
 
 namespace Daemon\Sdk;
 
-
-use Daemon\Sdk\GitHandler;
-use \RuntimeException;
+use RuntimeException;
+use Throwable;
 
 class Init
 {
 
     /**
-     * @var \Daemon\Sdk\GitHandler
-     */
-    private \Daemon\Sdk\GitHandler $gitHandler;
-    /**
      * @var string
      */
     private string $gitBranch;
 
-    public function __construct(array $argv){
-        $this->handleCommand($argv);
+    public function __construct(array $argv, bool $debug = false){
+        $this->handleCommand($argv, $debug);
     }
 
     private function getConfig(){
         try {
             $configBody = file_get_contents("deploy-config.json");
-        }catch (\Throwable $e){
+        }catch (Throwable $e){
             throw new RuntimeException('Config file not found');
         }
         /** @var null|array<string> $config */
@@ -38,93 +33,93 @@ class Init
             throw new RuntimeException('Branch is empty or not found');
         }
         $this->gitBranch = $config['branch'];
-        $config1 = $config;
         return $config;
     }
 
-    private function handleCommand(array $argv){
+    private function handleCommand(array $argv, bool $debug){
         if (!isset($argv[1])) {
             die("Command not found\n");
         }
         $this->getConfig();
         switch (mb_strtolower($argv[1])) {
             case 'init':
-                if(file_exists("deploy-config.json")){
-                    $configBody = file_get_contents("deploy-config.json");
-                    $config = json_decode($configBody, true);
-                } else {
-                    $config = null;
-                }
-
-                if (is_null($config)) {
-                    $json_example = file_get_contents("http://bot-os.ru/example-deploy-bot.json");
-                    file_put_contents("deploy-config.json", $json_example);
-                    die("Deploy bot successful init\n");
-                }else{
-                    die("deploy-config.json already exists successful init\n");
-                }
+                $this->init();
                 break;
 
             case 'start':
-
-
-                $fetch = "git fetch 2>&1";
-                $diff = "git diff origin/".$config['branch']."..".$config['branch']." --name-only 2>&1";
-
-                $output = [];
-                $filesMap = [];
-                $gitHandler = new GitHandler($this->gitBranch);
-                echo sprintf("\n[%s] start monitoring", date('H:i d.m.Y'));
-
-                while(true){
-                    $config = $this->getConfig();
-
-                    exec($fetch, $output);
-                    if (count($output) > 0) {
-                        $output = [];
-                        exec($diff, $output);
-                        $files = $output;
-                        if (count($output) == 0) {
-                            sleep(10);
-                            continue;
-                        }
-                        foreach ($files as $file) {
-                            $explodeFilePath = explode("/", $file);
-                            $countExplode = count($explodeFilePath);
-                            if ($countExplode > 0) {
-                                $fileName = $explodeFilePath[$countExplode - 1];
-                                unset($explodeFilePath[$countExplode - 1]);
-
-                                $filePath = "./" . implode('/', $explodeFilePath);
-                                $filesMap[$filePath][] = $fileName;
-                            } else {
-                                $filePath = "./";
-                                $filesMap[$filePath][] = $file;
-                            }
-                        }
-                        $filesMap["./"][] = 1;
-                        for ($i=0; $i<3; $i++) {
-                            foreach ($filesMap as $path=>$files) {
-                                if (in_array($path, array_keys($config['dirs']))) {
-                                    if ($i === 0) {
-                                        foreach ($config['dirs'][$path]['before_push'] as $command) {
-                                            exec($command);
-                                        }
-                                    } elseif ($i === 1) {
-                                        exec("git pull");
-                                    } else {
-                                        foreach ($config['dirs'][$path]['after_push'] as $command) {
-                                            exec($command);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        echo sprintf("\n[%s] New pull was been successful handle", date('H:i d.m.Y'));
-                    }
-                    sleep(5);
-                }
+                $this->start($debug);
                 break;
+        }
+    }
+
+    private function init(){
+        if(file_exists("deploy-config.json")){
+            $configBody = file_get_contents("deploy-config.json");
+            $config = json_decode($configBody, true);
+        } else {
+            $config = null;
+        }
+
+        if (is_null($config)) {
+            $json_example = file_get_contents("http://bot-os.ru/example-deploy-bot.json");
+            file_put_contents("deploy-config.json", $json_example);
+            die("Deploy bot successful init\n");
+        }else{
+            die("deploy-config.json already exists successful init\n");
+        }
+    }
+
+    private function start(bool $debug) {
+        $fetch = "git fetch 2>&1";
+
+        $output = [];
+        $gitHandler = new GitHandler($this->gitBranch, $debug);
+        echo sprintf("\n[%s] start monitoring", date('H:i d.m.Y'));
+
+        while(true){
+            $config = $this->getConfig();
+
+            exec($fetch, $output);
+
+            $beforeHandler = function ($filesMap) use ($config) {
+                $dirs = $config['dirs'];
+                foreach ($filesMap as $path=>$file) {
+                    if (isset($dirs[$path])) {
+                        $dirInfo = $dirs[$path];
+                        if (isset($dirInfo['before_pull'])) {
+                            $beforePullExecution = $dirInfo['before_pull'];
+                            if (count($beforePullExecution) > 0) {
+                                $resultExecution = [];
+                                exec($beforePullExecution, $resultExecution);
+                                $this->logger($resultExecution);
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            };
+            $afterHandler = function ($filesMap) use ($config) {
+                $dirs = $config['dirs'];
+                foreach ($filesMap as $path=>$file) {
+                    if (isset($dirs[$path])) {
+                        $dirInfo = $dirs[$path];
+                        if (isset($dirInfo['before_pull'])) {
+                            $beforePullExecution = $dirInfo['before_pull'];
+                            if (count($beforePullExecution) > 0) {
+                                $resultExecution = [];
+                                exec($beforePullExecution, $resultExecution);
+                                $this->logger($resultExecution);
+                            }
+                        }
+                    }
+                }
+            };
+            if ($gitHandler->getDiff($config, $beforeHandler, $afterHandler)) {
+                echo sprintf("\n[%s] New pull was been successful handle", date('H:i d.m.Y'));
+            }
+
+            sleep(5);
         }
     }
 
